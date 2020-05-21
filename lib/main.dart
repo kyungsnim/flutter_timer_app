f936f551a4590918d859809d7a96bb7325195d06
kyungsnim <kyungsnim@kakao.com>
2020-05-21 23:52:39 +0900

    타이머앱 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -9,109 +10,202 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Stop watch',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StopWatchPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class StopWatchPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _StopWatchPageState createState() => _StopWatchPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _StopWatchPageState extends State<StopWatchPage> {
+  var _icon = Icons.play_arrow;
+  var _color = Colors.amber;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Timer _timer; // 타이머
+  var _time = 0; // 실제 늘어날 시간
+  var _isPlaying = false; // 시작/정지 상태값
+  List<String> _saveTimes = []; // 기록하기 위한 리스트
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Stop watch'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: _body(),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          _click();
+        }),
+        child: Icon(_icon),
+        backgroundColor: _color,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  // 시간 보여지는 부분
+  Widget _body() {
+    var sec = _time ~/ 100; // 초
+    var hundredth = '${_time % 100}'.padLeft(2, '0');
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Stack( // 위젯 위에 위젯 겹치기 가능
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text( // seconds 영역
+                      '$sec',
+                      style: TextStyle(fontSize: 80),
+                    ),
+                    Text( // milliseconds 영역
+                      '.$hundredth',
+                      style: TextStyle(fontSize: 30),
+                    )
+                  ],
+                ),
+                SizedBox(height: 50, // 여백
+                ),
+                Container(
+                  width: 200,
+                  height: 300,
+                  child: ListView(
+                    children: _saveTimes.map((time) => Text(time, style: TextStyle(fontSize: 20),)).toList(),
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Positioned(
+              right: 0,
+              bottom: 80,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _reset();
+                  });
+                },
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Color(0xFF0D47A1),
+                        Color(0xFF1976D2),
+                        Color(0xFF42A5F5),
+                      ],
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Clear Board',
+                    style: TextStyle(
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              bottom: 20,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _saveTime('$sec.$hundredth');
+                  });
+                },
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Color(0xFF0D47A1),
+                        Color(0xFF1976D2),
+                        Color(0xFF42A5F5),
+                      ],
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'Save Time!!',
+                    style: TextStyle(
+                        fontSize: 20),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+  // 시작, 일시정지 버튼
+  void _click() {
+    _isPlaying = !_isPlaying;
+
+    if(_isPlaying) {
+      _icon = Icons.pause;
+      _color = Colors.grey;
+      _start();
+    } else {
+      _icon = Icons.play_arrow;
+      _color = Colors.amber;
+      _pause();
+    }
+  }
+
+  // 1/100초에 한 번씩 time 1씩 증가
+  void _start() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  // 타이머 중지(취소)
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  // 초기화
+  void _reset() {
+    setState(() {
+      _isPlaying = false;
+      _timer?.cancel();
+      _saveTimes.clear();
+      _time = 0;
+    });
+  }
+
+  // 기록하기
+  void _saveTime(String time) {
+    _saveTimes.insert(0, '${_saveTimes.length + 1}등 : $time');
   }
 }
